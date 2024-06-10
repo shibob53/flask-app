11c1ed09e0af4bc3aa10df6e50d4bdf12be7c111
Ali <nic.oleda.l.i.rae@gmail.com>
2024-06-10 23:26:32 +0300

    Initial Flask App

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 from flask import Flask, request, jsonify
+#from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+def driversetup():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # Run Selenium in headless mode
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("lang=en")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--incognito")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(options=options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined});")
+    return driver
+    
+app = Flask(__name__)
+ 
+@app.route('/y')
+def y():
+  driver = driversetup()
+  
+  return  'Hello from Koyeb'
 app = Flask(__name__)
 ld={} 
 @app.route('/user', methods=["POST"])
